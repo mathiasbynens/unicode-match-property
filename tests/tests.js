81b18279f576dc8b499a2391d82b3d248e18303e
Mathias Bynens <mathias@qiwi.be>
2017-04-15 12:11:05 +0200

    Remove `Full_Composition_Exclusion`
    https://github.com/tc39/proposal-regexp-unicode-property-escapes/commit/56b0b3e27397030525b79951468a3ebb0b0f6f19

--- tests/tests.js
+++ tests/tests.js
@@ -47,14 +47,6 @@ test(t => {
 		'ASCII_Hex_Digit'
 	);
 	t.deepEqual(
-		matchProperty('Comp_Ex'),
-		'Full_Composition_Exclusion'
-	);
-	t.deepEqual(
-		matchProperty('Full_Composition_Exclusion'),
-		'Full_Composition_Exclusion'
-	);
-	t.deepEqual(
 		matchProperty('Emoji_Modifier'),
 		'Emoji_Modifier'
 	);
@@ -68,6 +60,78 @@ test(t => {
 	);
 	t.throws(
 		() => matchProperty('emojicomponent'),
+		Error
+	);
+	t.throws(
+		() => matchProperty('Composition_Exclusion'),
+		Error
+	);
+	t.throws(
+		() => matchProperty('Expands_On_NFC'),
+		Error
+	);
+	t.throws(
+		() => matchProperty('Expands_On_NFD'),
+		Error
+	);
+	t.throws(
+		() => matchProperty('Expands_On_NFKC'),
+		Error
+	);
+	t.throws(
+		() => matchProperty('Expands_On_NFKD'),
+		Error
+	);
+	t.throws(
+		() => matchProperty('FC_NFKC_Closure'),
+		Error
+	);
+	t.throws(
+		() => matchProperty('Full_Composition_Exclusion'),
+		Error
+	);
+	t.throws(
+		() => matchProperty('Grapheme_Link'),
+		Error
+	);
+	t.throws(
+		() => matchProperty('Hyphen'),
+		Error
+	);
+	t.throws(
+		() => matchProperty('Other_Alphabetic'),
+		Error
+	);
+	t.throws(
+		() => matchProperty('Other_Default_Ignorable_Code_Point'),
+		Error
+	);
+	t.throws(
+		() => matchProperty('Other_Grapheme_Extend'),
+		Error
+	);
+	t.throws(
+		() => matchProperty('Other_ID_Continue'),
+		Error
+	);
+	t.throws(
+		() => matchProperty('Other_ID_Start'),
+		Error
+	);
+	t.throws(
+		() => matchProperty('Other_Lowercase'),
+		Error
+	);
+	t.throws(
+		() => matchProperty('Other_Math'),
+		Error
+	);
+	t.throws(
+		() => matchProperty('Other_Uppercase'),
+		Error
+	);
+	t.throws(
+		() => matchProperty('Prepended_Concatenation_Mark'),
 		Error
 	);
 	t.throws(
